9187d212ef7497601f8ab1df2f1b518730ac631a
pechyonkin <maxim.pechyonkin@gmail.com>
2023-01-18 18:48:15 -0500

    Implement Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,54 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+  @R0
+  D=M
+  @first
+  M=D // Create first variable
+
+  @R1
+  D=M
+  @second
+  M=D // Create second variable
+
+  @sum
+  M=0 // Initialize sum = 0
+
+  @i
+  M=0 // Initialize i = 0
+
+  @R2
+  M=0 // Initialize resulting memory register to 0
+
+(LOOP)
+  @i
+  D=M
+  @second
+  D=D-M // D = i - second
+  @STOP
+  D;JGE // Stop if i >= second, i.e. we added first so sum exactly second times
+
+  @sum
+  D=M
+  @first
+  D=D+M
+  @sum
+  M=D // sum = sum + first
+
+  @i
+  M=M+1 // i = i + 1
+  @LOOP
+  0;JMP
+
+(STOP)
+  @sum
+  D=M
+  @R2
+  M=D
+  @END
+  0;JMP // R2 = sum
+
+(END)
+  @END
+  0;JMP
