dc2cd7ce98e0837927f0e6d7084bced33feea67d
pechyonkin <maxim.pechyonkin@gmail.com>
2023-01-19 19:09:23 -0500

    Color screen in black once

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,53 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+  @8192
+  D=A
+  
+  @n
+  M=D // n = 8192 16-bit chunks to fill screen
+
+  @i
+  M=0 // to keep track of iteration
+  
+  @color
+  M=-1 // all 1s (two's complement) to fill a chunk with black
+
+  @SCREEN
+  D=A
+
+  @screenptr
+  M=D // pointer of current screen chunk
+
+(FILL)
+  @i
+  D=M
+
+  @n
+  D=D-M // i - n
+
+  @END
+  D;JGE // stop iterating if i >= n, i.e. i - n >= 0
+
+  @color
+  D=M
+
+  @screenptr
+  A=M
+  M=D // dereference screen pointer
+
+  @screenptr
+  M=M+1 // increment screen pointer
+
+  @i
+  M=M+1 // increment counter
+
+  @FILL
+  0;JMP
+
+(END)
+  @END
+  0;JMP
+
+
