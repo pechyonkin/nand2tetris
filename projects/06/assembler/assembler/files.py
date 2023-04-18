c169d44c9c5000c3d91d939e6517485d1988b1db
pechyonkin <maxim.pechyonkin@gmail.com>
2023-04-17 20:48:14 -0400

    Add initial assembler project structure and test.

---
+++ projects/06/assembler/assembler/files.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from typing import List
+
+
+def load_lines(path: Path) -> List[str]:
+    """ Load lines and drop whitespaces at the beginning and end of each file.
+
+    :param path: path to input .asm file
+    :return: list of lines in the file
+    """
+    with open(path) as f:
+        lines = f.readlines()
+    lines = [line.lstrip().rstrip() for line in lines]
+    return lines
+
+
+print("This is good!")
---
+++ projects/06/assembler/tests/test_files.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import pytest
+
+from assembler.files import load_lines
+
+PROJECT_PATH = Path("/Users/maxbrut/git/nand2tetris/projects/06")
+
+ADD_PATH = PROJECT_PATH / "add" / "Add.asm"
+
+EXPECTED_ADD_LINES = [
+    "// This file is part of www.nand2tetris.org",
+    "// and the book \"The Elements of Computing Systems\"",
+    "// by Nisan and Schocken, MIT Press.",
+    "// File name: projects/06/add/Add.asm",
+    "",
+    "// Computes R0 = 2 + 3  (R0 refers to RAM[0])",
+    "",
+    "@2",
+    "D=A",
+    "@3",
+    "D=D+A",
+    "@0",
+    "M=D",
+]
+
+
+def test_load_lines():
+    with pytest.raises(IsADirectoryError):
+        load_lines(path=Path("/"))
+
+
+def test_load_add():
+    lines = load_lines(path=ADD_PATH)
+    assert lines == EXPECTED_ADD_LINES
