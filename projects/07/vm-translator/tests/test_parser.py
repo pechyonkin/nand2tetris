9fb4b47832674a61eb5868e5c33dbfad5fbde824
pechyonkin <maxim.pechyonkin@gmail.com>
2023-05-14 19:26:26 -0400

    Load and clean vm lines from file + tests.

--- projects/07/vm-translator/tests/test_parser.py
+++ projects/07/vm-translator/tests/test_parser.py
@@ -3,7 +3,39 @@ from pathlib import Path
 from translator.parser import load_vm_lines
 from main import PATHS_TO_PROCESS
 
+TEST_PATH_1 = Path(
+    "/Users/maxbrut/git/nand2tetris/projects/07/MemoryAccess/BasicTest/BasicTest.vm"
+)
+
+EXP_LINES_1 = [
+    "push constant 10",
+    "pop local 0",
+    "push constant 21",
+    "push constant 22",
+    "pop argument 2",
+    "pop argument 1",
+    "push constant 36",
+    "pop this 6",
+    "push constant 42",
+    "push constant 45",
+    "pop that 5",
+    "pop that 2",
+    "push constant 510",
+    "pop temp 6",
+    "push local 0",
+    "push that 5",
+    "add",
+    "push argument 1",
+    "sub",
+    "push this 6",
+    "push this 6",
+    "add",
+    "sub",
+    "push temp 6",
+    "add",
+]
+
 
 def test_load_vm_lines() -> None:
-    lines = load_vm_lines(path=Path(PATHS_TO_PROCESS[0]))
-    assert lines == []
+    lines = load_vm_lines(path=TEST_PATH_1)
+    assert lines == EXP_LINES_1
--- projects/07/vm-translator/translator/parser.py
+++ projects/07/vm-translator/translator/parser.py
@@ -2,5 +2,25 @@ from pathlib import Path
 from typing import List
 
 
+def cleanse_lines(lines: List[str]) -> List[str]:
+    out_lines = []
+    for line in lines:
+        if line.startswith("/"):
+            # ignore full comment lines
+            continue
+        if "//" in line:
+            # remove comments after code
+            line = line.split("//")[0]
+        # remove whitespace including new line chars
+        line = line.lstrip().rstrip()
+        if line:
+            # will ignore empty lines
+            out_lines.append(line)
+    return out_lines
+
+
 def load_vm_lines(path: Path) -> List[str]:
-    return []
+    with open(path) as f:
+        raw_lines = f.readlines()
+    clean_lines = cleanse_lines(lines=raw_lines)
+    return clean_lines
