0d51193f1bdcd1f363cd4583b2daa33be93520f8
pechyonkin <maxim.pechyonkin@gmail.com>
2023-05-23 19:56:08 -0400

    Implement pop_from_stack().

--- projects/07/vm-translator/translator/assemby.py
+++ projects/07/vm-translator/translator/assemby.py
@@ -4,6 +4,28 @@ Methods for converting VM code to assembly instructions.
 from typing import List
 
 
+def pop_from_stack(store_in_d: bool = True) -> List[str]:
+    """Pop the value from top of stack and optionally store in register D.
+    Popping is done via decrementing stack pointer SP by 1 and setting address
+    register A to point to the decremented value of stack pointer. So, after pop
+    is done, SP essentially points at the popped value, so the value can be
+    accessed by calling register M.
+
+    By default, this will also store the value in register D.
+
+    :param store_in_d: whether to store in register D.
+    :return: list of assembly to achieve "pop from stack" operation.
+    """
+    assembly = [
+        "@SP",
+        "M=M-1",
+        "A=M",
+    ]
+    if store_in_d:
+        assembly.append("D=M")
+    return assembly
+
+
 def push_constant(value: str) -> List[str]:
     """Push a constant 'value' onto the stack."""
     assembly: List[str] = [
